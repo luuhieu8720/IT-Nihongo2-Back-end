e32ed329aa2e691995633cb525c111c6a09fd6fc
dragoon012234 <dragoon012234@gmail.com>
2021-10-21 21:11:57 +0700

    add support method put for update

--- src/main/java/bkdn/pbl6/main/controllers/generic/GenericController.java
+++ src/main/java/bkdn/pbl6/main/controllers/generic/GenericController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,7 +62,7 @@ public class GenericController {
 		}
 	}
 
-	@PostMapping(path = "/update")
+	@RequestMapping(path = "/update", method = { RequestMethod.POST, RequestMethod.PUT })
 	public ResponseEntity<ApiResponse> apiUpdate(@RequestBody Data data) {
 		AccountModel accountModel = getAccount();
 		data.setUsername(accountModel.getUsername());
